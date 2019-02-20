af75ce4e3d66f9ebc286da71ffbe8322f0875a5c
ChrisCocco <ChrisCocco@users.noreply.github.com>
2019-02-20 18:29:45 +0100

    From html to SQL, base A1, table savants

--- read_tab_file.py
+++ read_tab_file.py
@@ -18,27 +18,48 @@ dataA1 = dfs[0]
 printout = str()
 
 printout = "INSERT INTO Savants (id_savant, type_savant, nom, prenom, " +\
-	"naissance_date, naissance_date_certitude) VALUES\n"
+	"naissance_date, naissance_date_certitude, naissance_date_comment," +\
+	"mort_date, mort_date_certitude, mort_date_comment, naissance_lieu,"+\
+	"mort_lieu) VALUES\n"
 
 for index, row in dataA1.iterrows():
 	name = str(row['Nom'])
+	if name == "nan":
+		continue
 	name = name.replace("'", "\\\'")
+
 	firstname = str(row['Prénom'])
 	firstname = firstname.replace("'", "\\\'")
+
 	born_date = str(row['Né'])
 	if re.match('^[0-9]{4}$', born_date):
 		born_date_cert = str(1)
+		born_comment   = str()
 	else:
 		born_date_cert = str(0)
-		born_date = born_date[0:4]
-		born_comment = born_date[4:]
+		born_comment   = born_date[5:]
+		born_date      = born_date[0:4]
+
+	death_date = str(row['Mort'])
+	if re.match('^[0-9]{4}$', death_date):
+		death_date_cert = str(1)
+		death_comment   = str()
+	else:
+		death_date_cert = str(0)
+		death_comment   = death_date[5:]
+		death_date      = death_date[0:4]
+
+	born_place  = str(row['Lieu naiss.'])
+	born_place  = born_place.replace("'", "\\\'")
+	death_place = str(row['Lieu mort'])
+	death_place = death_place.replace("'", "\\\'")
 		
 	printout += '(\'' + str(row['Numéro']) + '\', \'A\',\'' + name + '\', \'' +\
-				 firstname + '\',' + born_date + ',' + born_date_cert +\
-				 '),\n'
-    # !!!!!!!
-    # need to check empty rows (no name and firstname, birthdate, and so on) !!!!!!!!!!!!!!
-    # !!!!!!!
+				 firstname + '\',' + born_date + ',' + born_date_cert + ',\''+\
+				 born_comment + '\',' + death_date + ',' + death_date_cert +\
+				 ',\'' + death_comment + '\', \'' + born_place + '\', \'' +\
+				 death_place + '\'),\n'
+
 
 
 # COULD BE A SOLUTION FOR SPECIAL CHARACTER such as ' \' '
