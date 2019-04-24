de1ee723c49ed42b12abcd9389243416ecd6ba28
ChrisCocco <ChrisCocco@users.noreply.github.com>
2019-04-24 16:58:36 +0200

    Html to SQL, base A2 -> table savants, minimal done

--- read_tab_file.py
+++ read_tab_file.py
@@ -10,11 +10,11 @@ dfsA2  = pd.read_html('DATA/BaseA2.htm', header = 0)
 dataA1 = dfsA1[0]
 dataA2 = dfsA2[0]
 
-#dataA  = pd.concat([dataA1, dataA2], sort = False)
+dataA  = pd.concat([dataA1, dataA2], sort = False)
 # Two columns do not have the same name in both databases: 
 # "Corrections" for A1 and "Corr. BMS" for A2 (no prob, not used)
 
-dataA = dataA1
+#dataA = dataA1
 
 
 # print(dataA1.loc[:,"Numéro"])
@@ -35,7 +35,7 @@ for index, row in dataA.iterrows():
 	id_savant = str(row['Numéro']).zfill(4)
 
 	name = str(row['Nom'])
-	if name == "nan":
+	if name == "nan" or name == "vide":
 		continue
 	name = name.replace("'", "\\\'")
 
@@ -45,7 +45,7 @@ for index, row in dataA.iterrows():
 	born_date = str(row['Né'])
 	if re.match('^[0-9]{4}$', born_date):
 		born_date_cert = str(1)
-		born_comment   = str()
+		born_comment   = 'NULL'
 	else:
 		born_date_cert = str(0)
 		born_comment   = born_date[5:]
@@ -54,16 +54,23 @@ for index, row in dataA.iterrows():
 	death_date = str(row['Mort'])
 	if re.match('^[0-9]{4}$', death_date):
 		death_date_cert = str(1)
-		death_comment   = str()
+		death_comment   = 'NULL'
 	else:
 		death_date_cert = str(0)
 		death_comment   = death_date[5:]
 		death_date      = death_date[0:4]
 
 	born_place  = str(row['Lieu naiss.'])
-	born_place  = born_place.replace("'", "\\\'")
+	if born_place == "nan":
+		born_place = 'NULL'
+	else:
+		born_place  = born_place.replace("'", "\\\'")
+	
 	death_place = str(row['Lieu mort'])
-	death_place = death_place.replace("'", "\\\'")
+	if death_place == "nan":
+		death_place = 'NULL'
+	else:
+		death_place = death_place.replace("'", "\\\'")
 
 	discipl_1   = str(row['Discipl. 1'])
 	discipl_1   = discipl_1.replace("'", "\\\'")
@@ -72,8 +79,8 @@ for index, row in dataA.iterrows():
 	try:
 		nbre_acad   = str(int(row['Nbre acad.']))
 	except:
-		print("Prob about nbre_acad with ", id_savant, row['Nbre acad.'])
-		nbre_acad   = str(99) # TO MODIFY!!!!!!!!!!!!!!
+		print("Prob about nbre_acad with ", id_savant, row['Nbre acad.'], "NULL introduced")
+		nbre_acad   = 'NULL'
 
 	if str(row['Gasc.']) == "oui":
 		gasc_bin = 1
@@ -90,16 +97,19 @@ for index, row in dataA.iterrows():
 		type_eminence = 3
 	else:
 		print("Prob about eminence type with ", id_savant, str(row['AA et AAA']))
-		type_eminence = 5
 
 		
-	printout += '(\'' + id_savant + '\', \'A\',\'' + name + '\', \'' +\
+	printrow  = '(\'' + id_savant + '\', \'A\',\'' + name + '\', \'' +\
 				 firstname + '\',' + born_date + ',' + born_date_cert + ',\''+\
 				 born_comment + '\',' + death_date + ',' + death_date_cert +\
 				 ',\'' + death_comment + '\', \'' + born_place + '\', \'' +\
 				 death_place + '\', \'' + discipl_1 + '\',' +\
 				 nbre_acad + ',' + str(gasc_bin) + ',' + str(type_eminence) +\
 				 '),\n'
+
+	printrow  = printrow.replace("'NULL'", "NULL")
+
+	printout += printrow
 
 
 
--- small_tables.sql
+++ small_tables.sql
@@ -2,5 +2,4 @@ INSERT INTO Type_eminence (id_type_eminence, type_eminence) VALUES
 ('1', 'AAA'),
 ('2', 'AA'),
 ('3', 'A'),
-('4', 'B'),
-('5', 'NA')
+('4', 'B')
