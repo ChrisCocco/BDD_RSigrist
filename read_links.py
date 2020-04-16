109ff92845e8c2dba2fd29a97eb3fb0db66ecdba
ChrisCocco <ChrisCocco@users.noreply.github.com>
2020-04-16 20:55:34 +0200

    Html to SQL -> table Liens in progress...

--- read_links.py
+++ read_links.py
@@ -12,7 +12,7 @@ dataA1_base = dfsA1_base[0]
 
 printout   = str()
 
-printout = "INSERT INTO Liens (id_savant)\n"
+printout = "INSERT INTO Liens (id_savant, type_savant, id_savant_1, type_savant_1, id_type_lien, lien_intensite)\n"
 
 
 #################
@@ -23,10 +23,56 @@ for index, row in dataA1_base.iterrows():
 
 	id_savant = str(row['Numéro']).zfill(4)
 
-	printrow  = '(\'' + id_savant + '\', \'A\'' + '),\n'
+	#need loop on columns...!!! columns_M = ['Maître 1']
+	if pd.notna(row['Maître 1']):
+		savant_1 = str(row['Maître 1'])
+		elements = re.findall('\(.*?\)', savant_1)
+		if elements:
+			# only master with an intensity
+			if len(elements) == 1 and re.match('\(\dX\)',elements[0]):
+				id_elements = re.search('no (\d{4})(\-B)?', savant_1)
+				if id_elements:
+					id_savant_1 = id_elements.group(1)
+					relation_type = str(1)
+					lien_intensite = re.search('\d', elements[0]).group(0)
+					# if there is "-B" in the name
+					if id_elements.group(2):
+						type_savant_1 = "B"
+					else:
+						type_savant_1 = "A" 
+				# maybe add minimal savants...
+				else:
+					print(savant_1)
+			else:
+				# print("to be continued...")
+				id_savant_1 = "to be continued"
+				type_savant_1 = "to be continued"
+				relation_type = "to be continued"
+				lien_intensite = "to be continued"
+		else:
+			id_elements = re.search('no (\d{4})(\-B)?', savant_1)
+			if id_elements:
+				id_savant_1 = id_elements.group(1)
+				relation_type = str(1)
+				lien_intensite = str(1)
+				# if there is "-B" in the name
+				if id_elements.group(2):
+					type_savant_1 = "B"
+				else:
+					type_savant_1 = "A" 
+			# maybe add minimal savants...
+			else:
+				print(savant_1)
+	
 
-	printrow  = printrow.replace("'NULL'", "NULL")
+		printrow  = '(\'' + id_savant + '\', \'A\',\'' + id_savant_1 + '\', \'' + type_savant_1 + '\', \'' + relation_type + '\', \'' + lien_intensite + '),\n'
 
-	printout += printrow
+		printrow  = printrow.replace("'NULL'", "NULL")
+
+		printout += printrow
+
+	
 
 print(printout)
+#with open("data_for_sql/liens.sql", mode = "w", encoding = "utf8") as f: #mode "a" for the followings
+#	f.write(printout[:-2]) #-2 to remove the last ",\n"
