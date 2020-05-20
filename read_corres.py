3ff9dea75942c944d1a51cb1c4a032abb0425123
ChrisCocco <ChrisCocco@users.noreply.github.com>
2020-05-20 18:57:39 +0200

    Html to SQL -> table Correspondances IN PROGRESS...

--- read_corres.py
+++ read_corres.py
@@ -4,13 +4,22 @@ import pandas as pd
 import re
 import functools # to use "reduce"
 
+def string_print(id_sav, type_sav, id_sav_2, type_sav_2, weight):
+	return('(' + id_sav + ',\'' + type_sav + '\',' + id_sav_2 +\
+		', \'' + type_sav_2 + '\', \'' + weight + '\'),\n')
+
 dfsA  = pd.read_html('DATA/CorresA.htm', header = 0)
 dfsB = pd.read_html('DATA/CorresB.htm', header = 0)
 
 dataA = dfsA[0]
 dataB = dfsB[0]
 
+printout   = str()
 
+printout = "INSERT INTO Correspondances (id_savant, type_savant, id_savant_1, " +\
+	"type_savant_1, corresp_intensite) VALUES\n"
+
+# From CorresA
 
 colnames_corr    = ["Corr. " + str(i+1) for i in range(38)]
 colnames_corr_AB = ["Corr. B" + str(i+1) for i in range(32)]
@@ -25,32 +34,44 @@ for index, row in dataA.iterrows():
 			if pd.notna(row[corr]) and row[corr] != "-----":
 				parts = re.search(
 					'(.+)(?:,|.) no ([0-9]{4}(\-B)?) \(([0-9])X', 
-					row[corr]) 
-				#last bracket "\)" removed in regex, since once "(3X9" instead of "(3X)"
-				# Now, corrected, but not useful to change
-	#			print(parts)
+					row[corr])
 				if parts:
 					id_sav_2   = parts.group(2) 
-					weight     = int(parts.group(4))
+					weight     = parts.group(4)
 					type_sav_2 = "A"
 
 				else:
 					print(index, corr)
+
+				if int(id_sav_2) == 2053 or int(id_sav_2) == 1823 or int(id_sav_2) == 2023 or int(id_sav_2) == 1939:
+					continue
+				
+				printrow  = string_print(id_sav, type_sav, 
+					id_sav_2, type_sav_2, weight)
+
+				printout += printrow
+				
 		for corr in colnames_corr_AB:
 			if pd.notna(row[corr]) and row[corr] != "-----":
 				parts = re.search(
-					'(.+)(?:,|.) no ([0-9]{4}(\-B)?) \(([0-9])X', 
+					'(.+)(?:,|.) no ([0-9]{4})(\-B)? \(([0-9])X', 
 					row[corr]) 
-				#last bracket "\)" removed in regex, since once "(3X9" instead of "(3X)"
-				# Now, corrected, but not useful to change
-	#			print(parts)
 				if parts:
 					id_sav_2   = parts.group(2) 
-					weight     = int(parts.group(4))
+					weight     = parts.group(4)
 					type_sav_2 = "B"
 
 				else:
 					print(index, corr)
+
+				if int(id_sav_2) == 4796:
+					continue
+
+				printrow  = string_print(id_sav, type_sav, 
+					id_sav_2, type_sav_2, weight)
+
+				printout += printrow
+
 		if pd.notna(row['Autres corr. A']):
 			for scientist in row['Autres corr. A'].split(";"):
 				parts = re.search(
@@ -58,57 +79,81 @@ for index, row in dataA.iterrows():
 					scientist)
 				if parts:
 					id_sav_2   = parts.group(2)
-					weight     = int(parts.group(4))
+					weight     = parts.group(4)
 					type_sav_2 = "A"
 				else:
 					print(index,row['Autres corr. A'])
+
+				printrow  = string_print(id_sav, type_sav, 
+					id_sav_2, type_sav_2, weight)
+
+				printout += printrow
+
 		if pd.notna(row['Autres corr. B']):
 			for scientist in row['Autres corr. B'].split(";"):
 				parts = re.search(
-					'(.+), no ([0-9]{4}(\-B)?) \(([0-9])X', 
+					'(.+), no ([0-9]{4})(\-B)? \(([0-9])X', 
 					scientist)
 				if parts:
 					id_sav_2   = parts.group(2)
-					weight     = int(parts.group(4))
+					weight     = parts.group(4)
 					type_sav_2 = "B"
 				else:
 					print(index,row['Autres corr. B'])
 
+				printrow  = string_print(id_sav, type_sav, 
+					id_sav_2, type_sav_2, weight)
+
+				printout += printrow
+
+
+
+# From CorresB
 
 colnames_corrB    = ["Corr. " + str(i+1) for i in range(28)]
 colnames_corrB_BA = ["Corr. B" + str(i+1) for i in range(28)]
 
 for index, row in dataB.iterrows():
 	if pd.notna(row['Corr. 1']) or pd.notna(row['Corr. B1']):
-		id_sav   = str(row['Numéro']).zfill(4)
+		id_sav   = str(row['Numéro'])[0:4]
 		type_sav = "B"
 		for corr in colnames_corrB:
 			if pd.notna(row[corr]) and row[corr] != "-----":
 				parts = re.search(
 					'(.+), no ([0-9]{4}(\-B)?) \(([0-9])X', 
 					row[corr]) 
-				#last bracket "\)" removed in regex, since once "(3X9" instead of "(3X)"
-				# Now, corrected, but not useful to change
-	#			print(parts)
+
 				if parts:
 					id_sav_2   = parts.group(2) 
-					weight     = int(parts.group(4))
+					weight     = parts.group(4)
 					type_sav_2 = "A"
 				else:
 					print(index, corr)
+
+				printrow  = string_print(id_sav, type_sav, 
+					id_sav_2, type_sav_2, weight)
+
+				printout += printrow
 
 
 		for corr in colnames_corrB_BA:
 			if pd.notna(row[corr]) and row[corr] != "-----":
 				parts = re.search(
-					'(.+), no ([0-9]{4}(\-B)?) \(([0-9])X', 
+					'(.+), no ([0-9]{4})(\-B)? \(([0-9])X', 
 					row[corr]) 
-				#last bracket "\)" removed in regex, since once "(3X9" instead of "(3X)"
-				# Now, corrected, but not useful to change
-	#			print(parts)
+
 				if parts:
 					id_sav_2   = parts.group(2) 
-					weight     = int(parts.group(4))
+					weight     = parts.group(4)
 					type_sav_2 = "B"
 				else:
 					print(index, corr)
+
+				printrow  = string_print(id_sav, type_sav, 
+					id_sav_2, type_sav_2, weight)
+
+				printout += printrow
+
+#print(printout)
+with open("data_for_sql/corresp.sql", mode = "w", encoding = "utf8") as f:
+	f.write(printout[:-2]) #-2 to remove the last ",\n"
