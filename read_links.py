60b6caddaef1df92a520615c9ae9837bff8d05e5
ChrisCocco <ChrisCocco@users.noreply.github.com>
2020-07-27 21:40:24 +0200

    Html to SQL - table Liens, link master, in progress...

--- read_links.py
+++ read_links.py
@@ -29,20 +29,89 @@ for index, row in dataA1_base.iterrows():
 		elements = re.findall('\(.*?\)', savant_1)
 		if elements:
 			# only master with an intensity
-			if len(elements) == 1 and re.match('\(\dX\)',elements[0]):
+			if len(elements) == 1:
 				id_elements = re.search('no (\d{4})(\-B)?', savant_1)
-				if id_elements:
-					id_savant_1 = id_elements.group(1)
-					relation_type = str(1)
-					lien_intensite = re.search('\d', elements[0]).group(0)
-					# if there is "-B" in the name
-					if id_elements.group(2):
-						type_savant_1 = "B"
+
+				if re.match('\(\dX\)',elements[0]):
+					
+					if id_elements:
+						id_savant_1 = id_elements.group(1)
+						relation_type = str(1)
+						lien_intensite = re.search('\d', elements[0]).group(0)
+						# if there is "-B" in the name
+						if id_elements.group(2):
+							type_savant_1 = "B"
+						else:
+							type_savant_1 = "A" 
 					else:
-						type_savant_1 = "A" 
-				# maybe add minimal savants...
+						print(savant_1)
+
+				elif not re.match('\(\+.*\)',elements[0]):
+
+					if id_elements:
+						id_savant_1 = id_elements.group(1)
+
+						if elements[0] == "(patron)":
+							relation_type = str(3)
+						elif elements[0] == "(influence)":
+							relation_type = str(5)
+						else:
+							print(savant_1)
+						lien_intensite = str(1)
+						# if there is "-B" in the name
+						if id_elements.group(2):
+							type_savant_1 = "B"
+						else:
+							type_savant_1 = "A" 
+					else:
+						print(savant_1)
+
 				else:
-					print(savant_1)
+
+					if id_elements:
+						id_savant_1 = id_elements.group(1)
+						relation_type_a = str(1)
+						if elements[0] == "(+patron)":
+							relation_type_b = str(3)
+						elif elements[0] == "(+influence)":
+							relation_type_b = str(5)
+						else:
+							print(savant_1)
+						lien_intensite = str(1)
+						# if there is "-B" in the name
+						if id_elements.group(2):
+							type_savant_1 = "B"
+						else:
+							type_savant_1 = "A"
+
+						printrow  = '(\'' + id_savant +\
+							'\', \'A\',\'' +\
+							id_savant_1 + '\', \'' +\
+							type_savant_1 +\
+							'\', \'' + relation_type_a +\
+							'\', \'' + lien_intensite +\
+							'),\n'
+
+						printrow  = printrow.replace("'NULL'", "NULL")
+
+						printout += printrow
+
+						printrow  = '(\'' + id_savant +\
+							'\', \'A\',\'' +\
+							id_savant_1 + '\', \'' +\
+							type_savant_1 +\
+							'\', \'' + relation_type_b +\
+							'\', \'' + lien_intensite +\
+							'),\n'
+
+						printrow  = printrow.replace("'NULL'", "NULL")
+
+						printout += printrow
+
+						continue
+						
+					else:
+						print(savant_1)
 			else:
 				# print("to be continued...")
 				id_savant_1 = "to be continued"
@@ -60,10 +129,10 @@ for index, row in dataA1_base.iterrows():
 					type_savant_1 = "B"
 				else:
 					type_savant_1 = "A" 
-			# maybe add minimal savants...
 			else:
 				print(savant_1)
-	
+
+		# check if savant_1 is in the savants of the current database
 
 		printrow  = '(\'' + id_savant + '\', \'A\',\'' + id_savant_1 + '\', \'' + type_savant_1 + '\', \'' + relation_type + '\', \'' + lien_intensite + '),\n'
 
